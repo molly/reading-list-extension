2aff134ebba2ce8cd0f7910f7e84dfc4d1374d6c
Molly White <molly.white5@gmail.com>
2024-01-31 11:05:31 -0500

    add www, issue with axios sending auth on redirect

--- src/api/client.js
+++ src/api/client.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const API_URL =
   process.env === "local"
     ? "http://localhost:5001"
-    : "https://mollywhite.net/dynamic-api";
+    : "https://www.mollywhite.net/dynamic-api";
 
 const authInterceptorConfig = {
   requestRefresh: async (refreshToken) => {
